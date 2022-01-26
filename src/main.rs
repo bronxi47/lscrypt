f0cf5f0933ff140912f4722ea4a467468bf438e3
bronxi47 <hbenja47@gmail.com>
2022-01-26 09:29:52 -0300

    Add match in main fn to manage fn crypto_info return

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
 use clap::Parser;
 
 #[tokio::main]
-async fn crypto_info(name: String) -> Result<String, Box<dyn std::error::Error>> {
+async fn crypto_info(coin: String) -> Result<String, Box<dyn std::error::Error>> {
 
-    let resp = reqwest::get(format!("https://data.messari.io/api/v1/assets/{}/metrics", name))
+    let resp = reqwest::get(format!("https://data.messari.io/api/v1/assets/{}/metrics", coin))
         .await?
         .json::<serde_json::Value>()
         .await?;
-        let output = format!("USD {}", resp["data"]["market_data"]["price_usd"]);
+    let output = format!("{}", resp["data"]["market_data"]["price_usd"]);
 
-        Ok(output)
+    Ok(output)
 }
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Crypto to check price (btc,eth,sol,ada)
-    crypto: String,
-
+    crypt: String,
 }
 
 fn main() {
     let args = Args::parse();
-        println!("Checking {}...!", args.crypto);
-        let crypto = args.crypto;
-        println!("{:?}", crypto_info(crypto));
+    let crypto = args.crypt;
+    println!("Checking {}...!", crypto);
+    
+    match crypto_info(crypto) {
+        Ok(output) => println!("USD {}", output),
+        Err(e) => eprintln!("Sorry, something is wrong :( \n  {}", e),
+    }
 }
